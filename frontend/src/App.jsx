ecc3ce9d9e9a64c09d2d2314aa444ceafd3e6ffc
unknown <giorgiapridonidze08@gmail.com>
2025-09-27 10:21:04 +0400

    created basic logic of directing user to pathes based on if he is authorized or not

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,25 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 const App = () => {
 
+  const isAuthorized = false ; //temporary (replace it with cookie)
 
   const router = createBrowserRouter([
-    {path: _, element : _}
+  
+    {path: '/', element : isAuthorized ? <div>Main</div> : <div>Login</div>},//replace divs with components
+    {path: '/login', element : !isAuthorized && <div>Login</div>}//replace divs with components
+  
   ])
 
-
   return (
+    
     <div className="app-container">
+    
       <RouterProvider router={router}></RouterProvider>
+    
     </div>
-  )
-}
+  
+  );
+};
 
 
-export default App
+export default App;
