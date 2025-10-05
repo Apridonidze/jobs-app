d289658d8c5bc2d015448a2fc72a54395412c9ef
unknown <giorgiapridonidze08@gmail.com>
2025-10-05 22:00:08 +0400

    created LoginMessage.jsx toggle when user submits form + added navigation if user success form

--- frontend/src/components/LoginMessage.jsx
+++ frontend/src/components/LoginMessage.jsx
@@ -1,7 +1,36 @@
-const LoginMessage = ( {loginMessage ,isSuccesful ,setToggleLoginMessage}) => {
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+
+const LoginMessage = ( { loginMessage , isSuccesful , setToggleLoginMessage } ) => {
+    
+    const navigator = useNavigate()
+    const [seconds,setSeconds ] = useState(3)
+
+    useEffect(() => {
+       
+
+        const timer = setInterval(() => {
+        
+            setSeconds(seconds => {
+                if(seconds <= 0){clearInterval(timer) ;setToggleLoginMessage(false) ;return 0}
+                return seconds - 1
+        })
+        
+        }, 1000);
+
+       
+        if(seconds == 0){
+            if(isSuccesful){navigator('/', {replace: true})}
+            else return
+        }
+        return () => {clearInterval(timer)}
+        
+    },[seconds,navigator])
+
     return(
         <div className="login-message-container">
             {loginMessage}
+            {seconds}
 
         </div>
     )
