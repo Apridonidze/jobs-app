0423497f1919f5539bf3ba8ba24be00e7ddad351
unknown <giorgiapridonidze08@gmail.com>
2025-10-05 18:44:37 +0400

    created form layout of Login.jsx

--- frontend/src/containers/Login.jsx
+++ frontend/src/containers/Login.jsx
@@ -1,7 +1,40 @@
+import { useState } from "react"
+import { Link  } from "react-router-dom"
+
 const Login = () => {
+
+    const [email,setEmail] = useState('')
+    const [password,setPassword] = useState('')
+    
+
+    const handleLogin = () => {
+
+    }
+
     return(
         <div className="login-container">
             Login.jsx
+
+            <form onSubmit={handleLogin}>
+
+                <div className="form-floating">
+                    <input className="form-control" type="text" name="email" onChange={(e) => setEmail(e.target.value)} value={email} placeholder="Enter Your Login Email"/>
+                    <label htmlFor="email">Enter Your Login Email</label>
+                </div>
+
+                <div className="form-floating">
+                    <input className="form-control" type="password" name="password" onChange={(e) => setPassword(e.target.value)} value={password} placeholder="Enter Your Password"/>
+                    <label htmlFor="password">Enter Your Password</label>
+                </div>
+
+                <input type="submit" value="Login" />
+
+
+            </form>
+
+
+            <Link to='/sign'>Dont Have An Account?</Link>
+
         </div>
     )
 }
