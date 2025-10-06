c564fd19989a5ca00d88020cdd1917f0097c3629
unknown <giorgiapridonidze08@gmail.com>
2025-10-06 12:18:30 +0400

    created fetching function in MyUser.jsx to fetch user data for their profile

--- frontend/src/containers/MyUser.jsx
+++ frontend/src/containers/MyUser.jsx
@@ -1,7 +1,32 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { useCookies } from "react-cookie"
+
+
 const MyUser = () => {
+
+    
+    const MY_USER_API = 'http://localhost:8080/my-user' //move to .env
+
+    const [user,setUser] = useState(null)
+    
+    const [cookies, setCookies , removeCookies] = useCookies(['token'])
+
+    useEffect(() => {
+
+        axios
+        .get(MY_USER_API, {headers : {Authorization : `Bearer ${cookies.token}`}})
+        .then(resp => {
+            const userData = resp.data.data[0]
+            setUser({role : userData.user_role , name : userData.user_name, surname : userData.user_surname , birthDate : userData.user_birthdate, gender : userData.user_gender})
+        })
+        .catch(err => console.log(err))
+
+    },[])
+
     return (
         <div className="myuser-container">
-            my user
+            {user && user.name}
         </div>
     )
 }
