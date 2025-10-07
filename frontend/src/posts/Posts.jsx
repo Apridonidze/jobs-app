28ef79e8ffe99a9f0bc5649366d875e9bd8e05ca
unknown <giorgiapridonidze08@gmail.com>
2025-10-07 14:37:08 +0400

    added comments to add new features in posts

--- frontend/src/posts/Posts.jsx
+++ frontend/src/posts/Posts.jsx
@@ -2,6 +2,8 @@ import CreatePosts from './CreatePosts'
 const Posts = () => {
     return(
         <div className="posts-container">
+            
+            {/**toggle createPosts background here (dark background) so when user clicks it createpost will be closed (also clear input of createposts)*/}
             <CreatePosts />
             {/**add everyone elses posts here */}
         </div>
--- frontend/src/posts/PostsInput.jsx
+++ frontend/src/posts/PostsInput.jsx
@@ -1,24 +1,35 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 const PostsInput = () => {
 
+    let isValid
+    let data = {}
+
     const [postInput, setPostInput] = useState('')
+    const [postInputErr, setPostInputErr] = useState('')
+
+    const postInputRef = useRef(null)
 
     const SubmitPost = (e) => {
         e.preventDefault()
 
 //create fileters here 
-//if input is valid send to backend in posts route
-//then add post in posts with users username and etc
+//if input is valid send to backend in posts route with token headers and validate tokens in backend
+//then add post in posts with users username and etc //insert post create time and do not make user create new post if post is created 5 minutes ago
+        if(postInput.trim().length == 0){
 
+        }
 
     }
+
+
+    useEffect(() => {SubmitPost},[])
 
     return(
         <div className="posts-input-container container position-absolute border border-1 bg-white">
             <form onSubmit={SubmitPost}>
             
-                <input className="form-control" type="text" onChange={e => setPostInput(e.target.value)} value={postInput} placeholder="Add Your Post..."/>
+                <input className="form-control" type="text" onChange={e => setPostInput(e.target.value)} value={postInput} placeholder="Add Your Post..." ref={postInputRef}/>
                 <button className="btn btn-success">Create Post</button>
             
             </form>
