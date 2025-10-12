941cb05ed9b28b6b577857d88cd69e0f47491140
unknown <giorgiapridonidze08@gmail.com>
2025-10-12 20:48:42 +0400

    writted success status code in NewJobs.js (have to create table for jobs)

--- backend/Routes/NewJobs.js
+++ backend/Routes/NewJobs.js
@@ -1,21 +1,27 @@
 const express = require('express')
 const NewJobsRouter = express.Router()
 
+const db = require('../db/db')
+
 const JobsSchema = require('../schemas/JobsSchema')
 const verifyToken = require('../config/verifyToken')
+
+
 NewJobsRouter.get('/', (req,res) => {
     res.send('new jobs path')
 })
 
 
-NewJobsRouter.post('/', verifyToken, (req,res) => {
+NewJobsRouter.post('/', verifyToken, async (req,res) => {
 
-    
+
     const JobsResp = JobsSchema(req.body)
 
     if(!JobsResp.success){
-        return res.status(400).json('Invalid Input')
+        return res.status(400).json({err : 'Invalid Input'})
     }
+
+    return res.status(200).json({message : 'Valid Input'})
 
     //add success message here and post data into database 
 })
