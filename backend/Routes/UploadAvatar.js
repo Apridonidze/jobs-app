ef6df2f67a872e9c7a0ffc2197f35b9038bf464f
unknown <giorgiapridonidze08@gmail.com>
2025-10-13 13:02:41 +0400

    created post method in UploadAvatar.js and added comments  to do (have to make table in mysql for user_avatars)

--- backend/Routes/UploadAvatar.js
+++ backend/Routes/UploadAvatar.js
@@ -1,8 +1,21 @@
 const express = require('express')
+
+const verifyToken = require('../config/verifyToken')
+const db = require('../db/db')
+
 const UploadAvatarRouter = express.Router()
 
 UploadAvatarRouter.get('/' , (req,res) => {
     res.send('upload avatar path')
+
+})
+
+UploadAvatarRouter.post('/' ,verifyToken , async(req,res) => {
+    const [ rows ] = await db.query('select * from user_avatars where user_id = ?', [req.user.userId])
+
+    //if there is rows then send update 
+    //if there is not rows then send insert into 
+
 })
 
 
