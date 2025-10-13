cfc181cd0752aff0d0399c210167e1983f1ef2ad
unknown <giorgiapridonidze08@gmail.com>
2025-10-13 11:23:20 +0400

    created basic axios get request in MyJobs.jsx to fetch data from backend (have to create path in backend to send data to frontend)

--- frontend/src/createjobs/MyJobs.jsx
+++ frontend/src/createjobs/MyJobs.jsx
@@ -1,4 +1,17 @@
+import axios from "axios"
+import { useEffect } from "react"
+import { useCookies } from "react-cookie"
+
 const MyJobs = () => {
+
+    const [cookies,setCookies,removeCookies] = useCookies(['token'])
+
+    useEffect(() => {
+        axios.get('/add url here', {headers: {Authorization : `Bearer ${cookies.token}`}})
+        .then(resp => console.log(resp))
+        .catch(err => console.log(err))
+    },[])
+
     return (
         <div className="my-jobs-container">
             my jobs
