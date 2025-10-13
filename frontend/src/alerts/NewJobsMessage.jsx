bfcc581883fdf6578eec90f0661d443f5c69b09c
unknown <giorgiapridonidze08@gmail.com>
2025-10-13 10:50:36 +0400

    created timer for NewJobsMessage.jsx that triggers when seconds === 0 , when 0 and post is successfuly send alert message and input component is removed from screen else it shows alerts

--- frontend/src/alerts/NewJobsMessage.jsx
+++ frontend/src/alerts/NewJobsMessage.jsx
@@ -1,8 +1,40 @@
-const NewJobsMessage = () => {
-    //TODO :add timer for it like other alert components
+import { useState, useEffect } from "react";
+
+const NewJobsMessage = ( { isJobsSuccessful ,jobsMessage ,setToggleJobsMessage, setToggleCreateJobsInput } ) => {
+
+    const [seconds, setSeconds] = useState(3)
+    
+    
+        useEffect(() => {
+    
+            const handleTimer = setInterval(() => {
+                
+                setSeconds(seconds => {
+                    
+                    if(seconds <= 0){clearInterval(handleTimer) ;setToggleJobsMessage(false) ;return 0}
+                    return seconds - 1
+                
+                }) 
+    
+            }, 1000);
+            
+    
+            if(seconds == 0){
+                if(isJobsSuccessful){setToggleCreateJobsInput(false)}
+                else return
+            }
+            
+            
+    
+            return () =>  {clearInterval(handleTimer)}
+        
+    },[seconds])
+
     return (
         <div className="new-jobs-message-container container position-fixed bg-white top-0">
-            Job Message 
+            {isJobsSuccessful}
+            {jobsMessage}
+            {seconds}
         </div>
     )
 }
--- frontend/src/createjobs/CreateJobs.jsx
+++ frontend/src/createjobs/CreateJobs.jsx
@@ -17,7 +17,7 @@ const CreateJobs = ( {} ) => {
         <div className="create-jobs-container">
             <span>Create Job Opportunity</span> <button onClick={() => setToggleCreateJobsInput(!toggleCreateJobsInput)}>Add</button>
 
-            {toggleCreateJobsInput && <> <div className="create-jobs-input-background bg-dark opacity-50 position-fixed w-100 h-100 top-0 start-0" onClick={() => setToggleCreateJobsInput(false)}></div> <CreateJobsInput setIsJobsSuccessful={setIsJobsSuccessful} setToggleJobsMessage={setToggleJobsMessage} setJobsMessage={setJobsMessage}/> {toggleJobsMessage && <NewJobsMessage />} </> }
+            {toggleCreateJobsInput && <> <div className="create-jobs-input-background bg-dark opacity-50 position-fixed w-100 h-100 top-0 start-0" onClick={() => setToggleCreateJobsInput(false)}></div> <CreateJobsInput setIsJobsSuccessful={setIsJobsSuccessful} setToggleJobsMessage={setToggleJobsMessage} setJobsMessage={setJobsMessage}/> {toggleJobsMessage && <NewJobsMessage isJobsSuccessful={isJobsSuccessful} jobsMessage={jobsMessage} setToggleJobsMessage={setToggleJobsMessage} setToggleCreateJobsInput={setToggleCreateJobsInput} />} </> }
 
 {/* (recruiter side) add my posted jobs here */}
 
