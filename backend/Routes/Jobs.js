e14e31ec4402808524749b1bb20e2af07f81b69d
unknown <giorgiapridonidze08@gmail.com>
2025-10-13 11:50:09 +0400

    created query that fetches data from db to Jobs.js and sends status codes to frontend

--- backend/Routes/Jobs.js
+++ backend/Routes/Jobs.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const JobsRouter =  express.Router()
 
-JobsRouter.get('/',(req,res) => {
-    res.send('jobs path')
+const db = require('../db/db')
+
+JobsRouter.get('/', async (req,res) => {
+
+    const [ rows ] = await db.query('select * from jobs')
+
+    if(rows.length > 0) {
+        return res.status(200).json({message: 'Jobs Found', jobs : rows})
+    }
+
+    return res.status(400).json({error : 'No Jobs Found Yet'})
+
 })
 
 
