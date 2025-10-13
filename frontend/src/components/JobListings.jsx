4baa0c40f5a58faa9ce8d4733256f6e92a8000e7
unknown <giorgiapridonidze08@gmail.com>
2025-10-13 11:54:14 +0400

    fetched jobs in JobListing.jsx and displayed on screen from earliest to latest

--- frontend/src/components/JobListings.jsx
+++ frontend/src/components/JobListings.jsx
@@ -1,10 +1,27 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+
 const JobListings = () => {
 
-    //TODO : display all jobs here without any filters 
+    const JOBS_URL = 'http://localhost:8080/jobs'
+
+    const [jobs,setJobs] = useState([])
+    const [jobsErr,setJobsErr] = useState('')
+    
+    useEffect(() => {
+        axios
+        .get(JOBS_URL)
+        .then(resp => {setJobs(resp.data.jobs)})
+        .catch(err => setJobsErr(err.response.data.error))
+    },[])
 
     return (
-        <div className="job-listing-container">
+        <div className="job-listing-container d-flex flex-column">
             Job Listings
+            {jobs.map((e,i) => (
+                <span key={i}>{e.job_title}</span>
+            ))}
         </div>
     )
 }
