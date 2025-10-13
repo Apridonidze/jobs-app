bec3880f15c0a1ee8c0f5c1953371f4194724442
unknown <giorgiapridonidze08@gmail.com>
2025-10-13 11:21:30 +0400

    created MyJobs.jsx componnent in createjobs folder to display jobs which is created by you (recruiter)

--- frontend/src/createjobs/CreateJobs.jsx
+++ frontend/src/createjobs/CreateJobs.jsx
@@ -1,11 +1,11 @@
 import { useRef, useState } from "react"
+
 import CreateJobsInput from "./CreateJobsInput"
 import NewJobsMessage from "../alerts/NewJobsMessage"
+import MyJobs from "./MyJobs"
+
 
 const CreateJobs = ( {} ) => {
-
-
-    //TODO (recruiter side):when timer is stopped in NewJobsMessage.jsx make this page toggle down 
 
     const SubmitBtnRef = useRef(null)
     
@@ -19,8 +19,8 @@ const CreateJobs = ( {} ) => {
             <span>Create Job Opportunity</span> <button onClick={() => setToggleCreateJobsInput(!toggleCreateJobsInput)}>Add</button>
 
             {toggleCreateJobsInput && <> <div className="create-jobs-input-background bg-dark opacity-50 position-fixed w-100 h-100 top-0 start-0" onClick={() => setToggleCreateJobsInput(false)}></div> <CreateJobsInput SubmitBtnRef={SubmitBtnRef} setIsJobsSuccessful={setIsJobsSuccessful} setToggleJobsMessage={setToggleJobsMessage} setJobsMessage={setJobsMessage}/> {toggleJobsMessage && <NewJobsMessage  isJobsSuccessful={isJobsSuccessful} jobsMessage={jobsMessage} setToggleJobsMessage={setToggleJobsMessage} setToggleCreateJobsInput={setToggleCreateJobsInput} SubmitBtnRef={SubmitBtnRef}/>} </> }
-
-{/* (recruiter side) add my posted jobs here */}
+            
+            <MyJobs />
 
         </div>
     )
---
+++ frontend/src/createjobs/MyJobs.jsx
@@ -0,0 +1,9 @@
+const MyJobs = () => {
+    return (
+        <div className="my-jobs-container">
+            my jobs
+        </div>
+    )
+}
+
+export default MyJobs
