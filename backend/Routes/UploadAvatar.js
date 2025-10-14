e971d62041d6797d7e01519222681808b4be6762
unknown <giorgiapridonidze08@gmail.com>
2025-10-14 12:36:45 +0400

    created checking feature in UploadAvatar.js , if user already has avatar picture and wants to renew it update command is send to mysql , else if user doesnot have avatar insert command is send to mysql

--- backend/Routes/UploadAvatar.js
+++ backend/Routes/UploadAvatar.js
@@ -2,10 +2,19 @@ const express = require('express')
 
 const fs = require('fs')
 const multer = require('multer')
+const cors = require('cors')
 const verifyToken = require('../config/verifyToken')
 const db = require('../db/db')
 
 const UploadAvatarRouter = express.Router()
+
+
+const corsOption = {
+    origin: 'http://localhost:5173', 
+    methods : ["GET", 'POST'] ,
+    credentials : true
+}
+UploadAvatarRouter.use(cors(corsOption))
 
 const upload = multer({dest: '/uploads'})
 
@@ -17,18 +26,25 @@ UploadAvatarRouter.post('/' , verifyToken, upload.single('profile-picutre'), asy
 
         if(!file) return res.status(400).json({error: 'no image provided'})
 
-        //add file type filter here with if statements 
-
-        //check if user already has avatar pickture
-        //if so then send update column
-        //if not use insert new column
 
         fs.readFile(file.path , async(err, data) => {
             if(err) return res.status(500).send('error reading image')
 
-            const [ rows ] = await db.query('insert into user_avatar (user_id, user_avatar_content) values (?,?)' , [req.user.userId, data])
 
-            return res.status(200).json({message : 'avatar inserted successfully'})
+            const [ rows ] = await db.query('select * from user_avatar where user_id = (?)', [req.user.userId])
+
+            if(rows.length < 1){
+
+                const [ insertRows ] = await db.query('insert into user_avatar (user_id, user_avatar_content) values (?,?)' , [req.user.userId, data])
+                return res.status(200).json({message : 'avatar inserted successfully'})
+            }
+
+            const [ updateRows ] = await db.query('update user_avatar set user_avatar_content = ? where user_id = ?',[data, req.user.userId])
+
+
+            return res.status(200).json('row updates successfully')
+
+
     
         })
 
