40e0feeca1f18af0f3564375fc1d60ec87488e13
unknown <giorgiapridonidze08@gmail.com>
2025-10-15 21:10:54 +0400

    created post route in UploadDesc.jsx and tested connection between frontend and backend  (successsfully)

--- backend/Routes/UploadDesc.js
+++ backend/Routes/UploadDesc.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const verifyToken = require('../config/verifyToken')
 const UploadDescRouter = express.Router()
 
 
 UploadDescRouter.get('/', (req,res) => {
     res.send('upload desc path')
+})
+
+UploadDescRouter.post('/', verifyToken, async(req,res) => {
+
+    console.log(req.body)
+
 })
 
 
