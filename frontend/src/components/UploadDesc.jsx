6561b6d056b9d2e94d046ff7aa6775108abe36cb
unknown <giorgiapridonidze08@gmail.com>
2025-10-15 21:09:22 +0400

    added upload-desc path to UploadDesc.jsx component to send data via axios to backend (have to make post method function in backend/uploaddesc)

--- frontend/src/components/UploadDesc.jsx
+++ frontend/src/components/UploadDesc.jsx
@@ -10,7 +10,7 @@ const UploadDesc = () => {
     const descRef = useRef(null)
     const btnRef = useRef(null)
 
-    const POST_USER_DESC_URL = '' //add server url here
+    const POST_USER_DESC_URL = 'http://localhost:8080/upload-desc' //add server url here
 
     const [cookies,setCookies,removeCookies] = useCookies(['token'])
 
