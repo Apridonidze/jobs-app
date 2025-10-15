e893ae92c174b9e0bbb63eaf069243ed8e58024e
unknown <giorgiapridonidze08@gmail.com>
2025-10-15 21:05:37 +0400

    created post method requrest to server in UploadDesc.jsx if user passed valid input (have to add path for it in backend)

--- frontend/src/components/UploadDesc.jsx
+++ frontend/src/components/UploadDesc.jsx
@@ -1,4 +1,6 @@
+import axios from 'axios'
 import { useRef, useState } from "react"
+import { useCookies } from 'react-cookie'
 
 const UploadDesc = () => {
 
@@ -10,15 +12,25 @@ const UploadDesc = () => {
 
     const POST_USER_DESC_URL = '' //add server url here
 
+    const [cookies,setCookies,removeCookies] = useCookies(['token'])
+
     const SubmitDesc = (e) => {
 
         e.preventDefault()
 
         let isValid 
+        let data = {}
 
         if(desc.trim().length < 20){isValid = false ; setDescErr('Description Length is Too Small'); descRef.current.classList.add('is-invalid'); btnRef.current.classList.add('btn-danger');descRef.current.classList.remove('is-valid'); btnRef.current.classList.remove('btn-success')}
         else if (desc.trim() == ''|| desc.trim() == ' ' || desc == null || desc == undefined){isValid = false; setDescErr('Description Form Cant Be Empty'); descRef.current.classList.add('is-invalid'); btnRef.current.classList.add('btn-danger');descRef.current.classList.remove('is-valid'); btnRef.current.classList.remove('btn-success')}
-        else {isValid = true; setDescErr(''); descRef.current.classList.add('is-valid'); btnRef.current.classList.add('btn-success');descRef.current.classList.remove('is-invalid'); btnRef.current.classList.remove('btn-danger')}
+        else {isValid = true; setDescErr(''); descRef.current.classList.add('is-valid'); btnRef.current.classList.add('btn-success');descRef.current.classList.remove('is-invalid'); btnRef.current.classList.remove('btn-danger'); data = {...data,desc : desc}}
+
+
+        if(isValid){
+            axios.post(POST_USER_DESC_URL, data , {headers:{authorization: `bearer ${cookies.token}`}})
+            .then(resp => console.log(resp))
+            .catch(err => console.log(err))
+        }
 
 
 
