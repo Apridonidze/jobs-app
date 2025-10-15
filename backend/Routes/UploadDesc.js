1b74f32c201dcc8edb5c7775d650733c890a114c
unknown <giorgiapridonidze08@gmail.com>
2025-10-15 21:44:35 +0400

    integrated description validation schema in UploadDesc.js and added error message when erro happens that is send to frontend

--- backend/Routes/UploadDesc.js
+++ backend/Routes/UploadDesc.js
@@ -1,7 +1,9 @@
 const express = require('express')
-const verifyToken = require('../config/verifyToken')
 const UploadDescRouter = express.Router()
 
+const db = require('../db/db')
+const validateDesc = require('../schemas/DescSchema')
+const verifyToken = require('../config/verifyToken')
 
 UploadDescRouter.get('/', (req,res) => {
     res.send('upload desc path')
@@ -9,7 +11,12 @@ UploadDescRouter.get('/', (req,res) => {
 
 UploadDescRouter.post('/', verifyToken, async(req,res) => {
 
-    
+    const descResp = validateDesc(req.body)
+
+    if(!descResp.success){
+        return res.status(400).json({err : 'Invalid Input'})
+    }
+
 
 })
 
