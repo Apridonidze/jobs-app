f39a9b656c52f0081dff70cadcfc798d6dee2595
unknown <giorgiapridonidze08@gmail.com>
2025-10-17 19:50:13 +0400

    craeted UploadTags.jsx in components

---
+++ frontend/src/components/UploadTags.jsx
@@ -0,0 +1,10 @@
+const UploadTags = () => {
+    return (
+        <div className="upload-tags-container">
+            {/**fetch tags from server jhere if none return no tags yet*/}
+        </div>
+    )
+}
+
+
+export default UploadTags
