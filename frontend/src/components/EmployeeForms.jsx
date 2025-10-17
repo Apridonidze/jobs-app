d59f63e3485e1b7df09e6554b5d777cffb19301e
unknown <giorgiapridonidze08@gmail.com>
2025-10-17 21:14:01 +0400

    created EmployeeForms.jsx in components (to display forms that are valid for employeee to fill to find job effectively)

---
+++ frontend/src/components/EmployeeForms.jsx
@@ -0,0 +1,10 @@
+const EmployeeForms = () => {
+    return(
+        <div className="employee-forms-container">
+            Employee Forms
+        </div>
+    )
+}
+
+
+export default EmployeeForms
