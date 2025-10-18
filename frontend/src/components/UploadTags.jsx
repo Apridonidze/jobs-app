b3300a46a609914e9104f564d220ce5ebdf773a1
unknown <giorgiapridonidze08@gmail.com>
2025-10-18 12:58:36 +0400

    created basic form of UploadTags.jsx , added upload button (have to add functionality to it) , added tags removal (when tag span is clicked it is deleted from array)

--- frontend/src/components/UploadTags.jsx
+++ frontend/src/components/UploadTags.jsx
@@ -1,8 +1,37 @@
+import { useState } from "react"
+
 const UploadTags = () => {
+
+    const [tags,setTags] = useState([])
+
+    const handleTags = (e) => {
+        e.preventDefault()
+    }
+
     return (
         <div className="upload-tags-container bg-white position-fixed">
-            Upload Tags
-            {/* get tags here and more options below to add up and sent to sewrver which will update previous tags if user has any */}
+            <h1>Add Tags To Attract Employees In Same Scope</h1>
+
+            <form onSubmit={handleTags}>
+               
+                <select onChange={(e) => setTags(tags => [...tags,e.target.value])}>
+                    <option value="Web Developing">Web Developing</option>
+                    <option value="Web Engineering">Web Engineering</option>
+                    <option value="Dekstop App Developing">Dekstop App Developing</option>
+                    <option value="Dekstop App Engineering">Dekstop App Engineering</option>
+                    <option value="Mobile App Developing">Mobile App Developing</option>
+                    <option value="Mobile App Engineering">Mobile App Engineering</option>
+                </select>
+
+                <div className="slected-tags">
+                    {tags.map((tag, tagId) => (
+                        <span key={tagId} onClick={() => setTags(tags.filter(t => t !== tag))}>{tag}</span>
+                    ))}
+                </div>
+
+                <input type="submit" value="Upload Tags" />
+
+            </form>
         </div>
     )
 }
