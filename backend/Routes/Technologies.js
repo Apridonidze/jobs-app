8edb3d3b279c8a77c06e342aa97c546be4d90149
unknown <giorgiapridonidze08@gmail.com>
2025-10-20 12:02:15 +0400

    implemented TechnologiesSchema in Technologies.js (validates user input from frontend (works perfectly))

--- backend/Routes/Technologies.js
+++ backend/Routes/Technologies.js
@@ -4,13 +4,22 @@ const TechRouter = express.Router()
 const verifyToken = require('../config/verifyToken')
 const rateLimiter = require('../config/rateLimiter')
 
+const TechnologiesSchema = require('../schemas/TechnologiesSchema')
+
 TechRouter.get('/user-technologies', (req,res) => {
     res.send('/technologies path')
 })
 
 
 TechRouter.post('/new-technologies', verifyToken, rateLimiter, (req,res) => {
-    console.log(req.body)
+    const TechResp = TechnologiesSchema(req.body)
+
+    if(!TechResp.success){
+        return res.status(400).json('invalid input')
+    }
+    
+    return res.status(200).json('valid data')
+
 })
 
 
