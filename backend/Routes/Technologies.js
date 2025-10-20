e0083c18bfe879197de8cb64d7948725d3cc2cf1
unknown <giorgiapridonidze08@gmail.com>
2025-10-20 11:49:39 +0400

    created post method in Teechnologies.js and added middlewares : verifyToken, rateLimiter to it to prevent unwanted user to access route and rateLimiter to prevent user from spamming

--- backend/Routes/Technologies.js
+++ backend/Routes/Technologies.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const TechRouter = express.Router()
 
+const verifyToken = require('../config/verifyToken')
+const rateLimiter = require('../config/rateLimiter')
 
+TechRouter.get('/', (req,res) => {
+    return res.status('/technologies path')
+})
+
+
+TechRouter.post('/', verifyToken, rateLimiter, (req,res) => {
+    console.log(req.body)
+})
 
 
 
