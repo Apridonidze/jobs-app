ec372b137c9a121163f6f0d12ab56ef117357f28
unknown <giorgiapridonidze08@gmail.com>
2025-10-21 12:52:27 +0400

    created user_roles statements in role.js , function checks user input , if its valid then it checks if user already has ddata in database , if so it sends update statements else it sends insertion statement and return response based on the operation

--- backend/Routes/Role.js
+++ backend/Routes/Role.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const RoleRouter = express.Router()
 
+const db = require('../db/db')
 const RoleSchema = require('../schemas/RoleSchema')
 const verifyToken = require('../config/verifyToken')
 const rateLimiter = require('../config/rateLimiter')
@@ -21,6 +22,17 @@ RoleRouter.post('/upload-roles' , verifyToken  , rateLimiter , async(req,res) =>
     try{
 
         const [ rows ] = await db.query('select * from user_roles where user_id = ?',[req.user.userId])
+
+        if(rows.length < 1){
+            await db.query('insert into user_roles (user_id , user_roles) values (?,?)' ,[req.user.userId , JSON.stringify(req.body.roles)])
+
+            return res.status(200).json('Role Inserted Successfully')
+        }
+
+        
+        await db.query('update user_roles set user_roles = ? where user_id = ?' ,[ JSON.stringify(req.body.roles), req.user.userId ])
+
+        return res.status(200).json('Role Updated Successfully Successfully')
 
     }catch(err){
         return res.status(500).json('Database Error')
