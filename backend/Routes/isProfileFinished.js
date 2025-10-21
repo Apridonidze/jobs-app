d7eb3e47d1fd096daf189e3244e8756a43a169a4
unknown <giorgiapridonidze08@gmail.com>
2025-10-21 13:25:42 +0400

    creates isprofilefinished.js in backend/routes where function gets all the data fromn , user checks if they have finished profile and sends status code based on user profile status

---
+++ backend/Routes/isProfileFinished.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const isProfileFinishedRouter = express.Router()
+
+const db = require('../db/db')
+const verifyToken = require('../config/verifyToken')
+
+isProfileFinishedRouter.get('/', verifyToken , async (req,res) => {
+
+    try{
+        
+        const [ hasRole ] = await db.query('select * from user_roles where user_id = ?',[req.user.userId])
+        const [ hasTags ] = await db.query('select * from user_tags where user_id = ?',[req.user.userId])
+        const [ hasTechnologies ] = await db.query('select * from user_technologies where user_id = ?',[req.user.userId])
+        const [ hasAvatar ] = await db.query('select * from user_avatar where user_id = ?',[req.user.userId])
+        const [ hasDesc ] = await db.query('select * from user_des where user_id = ?',[req.user.userId])
+
+        if(hasRole.length > 1 && hasTags.length > 1 && hasTechnologies.length > 1 && hasAvatar.length > 1 && hasDesc.length > 1){
+            return res.status(200).json('User Profile is Finished')
+        }
+            
+        return res.status(204).json('User Profile is not Finished')
+        
+
+    }catch(err){
+        return res.status(500).json('Database Error')
+    }
+
+})
+
+
+
+module.exports = isProfileFinishedRouter
