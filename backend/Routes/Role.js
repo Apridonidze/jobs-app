eefbf9d328cc77e5752e28f042e857ffd20a955c
unknown <giorgiapridonidze08@gmail.com>
2025-10-24 16:54:47 +0400

    created get method in backend/Role.js that checks user role based on their userId , if they have roles it returns data to frontend , else it return 204 status code and says no data yet, if there is database error it returns message for it

--- backend/Routes/Role.js
+++ backend/Routes/Role.js
@@ -6,9 +6,22 @@ const RoleSchema = require('../schemas/RoleSchema')
 const verifyToken = require('../config/verifyToken')
 const rateLimiter = require('../config/rateLimiter')
 
-RoleRouter.get('/my-roles' , (req, res) => {
-    res.send('role router default path')
-    //make fetching for user roles
+RoleRouter.get('/my-roles' , verifyToken ,  async(req, res) => {
+
+    try{
+
+        const [ rows ] = await db.query('select * from user_roles where user_id = ?' , [req.user.userId])
+
+        if(rows.length < 1){
+            return res.status(204).json("No Roles Yet")
+        }
+
+        return res.status(200).json({roles : rows})
+
+    }catch(err){
+        return res.status(500).json('Database Error')
+    }
+
 })
 
 
