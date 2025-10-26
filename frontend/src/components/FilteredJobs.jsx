7bbe5b0c214158e5e6975f6954cd1d4bcf372f3d
unknown <giorgiapridonidze08@gmail.com>
2025-10-26 18:57:51 +0400

    created basic filter of jobs that are generated for user based on their role and technologies they use in FilteredJobs.jsx

--- frontend/src/components/FilteredJobs.jsx
+++ frontend/src/components/FilteredJobs.jsx
@@ -8,14 +8,27 @@ const FilteredJobs = ( { jobs } ) => {
 
     useEffect(() => {
 
-        //take jobs jhere filter it based on job.tags and job.technologies and display mapped filtered jobs in div
+        console.log(jobs)
+        console.log(user)
+        //jobs.job_technologies
+        //jobs.job_technologies
 
-    },[])
+        //import my technologies from technologies.js 
+        //import my role from role.js
+        //change temprary variables into this fetched variables  
+
+
+        const filtered = jobs.filter(job => job.job_technologies.includes('TypeScript') && job.job_employeeList.includes('Figma Designer'))
+        setFilteredJobs(filtered)
+
+        console.log(filtered)
+        
+    },[jobs])
+
 
     return(
         <div className="filtered-jobs-container">
             <h1>Jobs For You: </h1>
-             
         </div>
     )
 }
