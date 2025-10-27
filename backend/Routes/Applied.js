e70c62c1a990ff10745803ce9900d8d6019a13c3
unknown <giorgiapridonidze08@gmail.com>
2025-10-27 12:45:05 +0400

    creawted Applied.js in backend/Applied.js to save jobs that are applied by employee

---
+++ backend/Routes/Applied.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const AppliedRouter = express.Router()
+
+AppliedRouter.get('/my-applied-jobs', (req,res) => {
+    
+})
+AppliedRouter.post('/post-my-applied-jobs', (req,res) => {
+
+})
+
+
+module.exports = AppliedRouter
