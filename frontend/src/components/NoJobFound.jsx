92586793207ec1417dbe3d161b3828f93b029168
unknown <giorgiapridonidze08@gmail.com>
2025-10-27 10:53:39 +0400

    added text in NoJobFound.jsx component

--- frontend/src/components/NoJobFound.jsx
+++ frontend/src/components/NoJobFound.jsx
@@ -1,7 +1,9 @@
 const NoJobFound = () => {
     return (
         <div className="no-job-found">
-
+            <h1>No Jobs Found For You</h1>
+            <h2>Wait Untill Somebody Will Post Job That Matches Your Profile</h2>
+            <h3>See All Jobs In Jobs Listings</h3>
         </div>
     )
 }
