eae889397a516b5ec543cf9a24b3afaf3abadfe6
unknown <giorgiapridonidze08@gmail.com>
2025-10-28 12:36:09 +0400

    added comments in backend/Applied.js

--- backend/Routes/Applied.js
+++ backend/Routes/Applied.js
@@ -2,10 +2,15 @@ const express = require('express')
 const AppliedRouter = express.Router()
 
 AppliedRouter.get('/my-applied-jobs', (req,res) => {
-    
+    //integrate beriftytoken
+    //check if user aplied for jos baed on their token
+    //return no jobs found if there is not rovs
+    //else return jobs user has applied and status form recuiter /not checked yet // seen user // acceped user //denied user
 })
 AppliedRouter.post('/post-my-applied-jobs', (req,res) => {
- //
+ //check user token
+ //check user input
+ //pushinto table
 })
 
 
