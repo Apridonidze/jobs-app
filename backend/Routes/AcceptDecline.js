4e6027b2664be7929f7a788874a22aaf85f7af1f
unknown <giorgiapridonidze08@gmail.com>
2025-11-01 13:48:21 +0400

    created injection query into sql database where it checks if recuruiter already sended response to employee , if so it returns ytou already sendt status to this user else it sends insert statement into database

--- backend/Routes/AcceptDecline.js
+++ backend/Routes/AcceptDecline.js
@@ -4,6 +4,7 @@ const AcceptDeclineRouter = express.Router()
 const AcceptDeclineSchema = require('../schemas/AcceptDeclineSchema')
 
 const rateLimiter = require('../config/rateLimiter')
+const db = require('../db/db')
 
 AcceptDeclineRouter.get('/', (req, res) => {
     res.send('accept or decline default path')
@@ -20,7 +21,18 @@ AcceptDeclineRouter.post('/accept-decline-employee', rateLimiter , verifyToken ,
 
     try{
 
+        const [ rows ] = await db.query('select * from AcceptedDeclined where applicant_id = ? and job_id = ?' , [req.body.applicant_id , req.body.job_id])
         
+
+        if(rows.length > 1){
+
+            console.log('you already sent status to this user') // add status code
+
+        }
+
+        await db.query('insert into AcceptedDeclined (job_id, applicant_id , status) values (?,?,?)' , [req.body.job_id, req.body.applicant_id , req.body.status])
+
+        console.log('response sended to database')
         
 
     }catch(err){
