9d8166b242b236a6f206577361dcd41b715a4dc9
unknown <giorgiapridonidze08@gmail.com>
2025-11-02 21:39:26 +0400

    created applicants fetching api to fetch applicants for specific job in JobHolder.jsx

--- frontend/src/createjobs/JobHolde.jsx
+++ frontend/src/createjobs/JobHolde.jsx
@@ -1,7 +1,26 @@
-const JobHolder = () => {
+const JobHolder = ( { job } ) => {
+
+
     return(
         <div className="job-holder-container">
+            <div className="job-info">
+                    
+                <h1>{job.job_title}</h1>
+                <h1>{job.job_id}</h1>
+                <h2>{job.job_desc}</h2>
+                <h3>{job.job_employeeList}</h3>
+                <h3>{job.job_technologies}</h3>
+                <h3>{job.job_languages}</h3>
 
+            </div>
+
+            <div className="job-applicants">
+                <h3>Applicants For This Job : </h3>
+            </div>
+
+            <div className="job-buttons">
+                <button>Delete</button>
+            </div>
         </div>
     )
 }
--- frontend/src/createjobs/SeeMore.jsx
+++ frontend/src/createjobs/SeeMore.jsx
@@ -1,19 +1,24 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
 import { useCookies } from "react-cookie"
+import JobHolder from "./JobHolde"
 
 const SeeMore = ( {toggleSeeMore} ) => {
 
     const [ cookies ] = useCookies(['token'])
     const [job, setJob] = useState(null)
+    const [applicants,setApplicants] = useState(null)
     const JOBS_URL = 'http://localhost:8080/jobs/user-jobs'
+    const APPLICANTS_URL = 'http://localhost:8080/applied/my-applicants'
 
     useEffect(() => {
         const fetchJob = async() => { 
             try{
                 await Promise.all([
                 axios.get(`${JOBS_URL}/${toggleSeeMore.job_id}`, {headers : {Authorization : `Bearer ${cookies.token}`}})
-                .then(resp => setJob(resp.data[0]))
+                .then(resp => {setJob(resp.data[0])}),
+                axios.get(APPLICANTS_URL , {headers : {Authorization : `Bearer ${cookies.token}`}})
+                .then(resp => console.log(resp))
             ])
             
             }catch(err){
@@ -24,18 +29,15 @@ const SeeMore = ( {toggleSeeMore} ) => {
         fetchJob()
     },[])
 
+    
+    console.log(job && job.job_id)
+
 
     //job_id, user_id, job_title, job_desc, job_employeeList, job_technologies, job_languages
 
     return(
         <div className="see-more-container position-fixed bg-white">
-            {job && <>
-                <h1>{job.job_title}</h1>
-                <h2>{job.job_desc}</h2>
-                <h3>{job.job_employeeList}</h3>
-                <h3>{job.job_technologies}</h3>
-                <h3>{job.job_languages}</h3>
-            </>}
+            {job && applicants && <JobHolder job={job} />}
         </div>
     )
 }
