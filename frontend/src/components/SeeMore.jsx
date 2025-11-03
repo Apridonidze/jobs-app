720757e45fa7f891a3e2513c2db5291b463a7bc5
unknown <giorgiapridonidze08@gmail.com>
2025-11-03 14:08:40 +0400

    fetched data via axios from backend api in SeeMore.jsx about job informartion

--- frontend/src/components/SeeMore.jsx
+++ frontend/src/components/SeeMore.jsx
@@ -1,7 +1,34 @@
-const SeeMore = () => {
+import axios from "axios"
+import { useEffect } from "react"
+import { useCookies } from "react-cookie"
+
+const SeeMore = ( { toggleSeeMore } ) => {
+
+    const [cookies] = useCookies(['token'])
+    const JOBS_URL = 'http://localhost:8080/jobs/user-jobs'
+
+    useEffect(() => {
+        
+        const fetchJob = async() => {
+
+            try{
+                await Promise.all([
+                    axios.get(`${JOBS_URL}/${toggleSeeMore.job_id}`, {headers : {Authorization : `Bearer ${cookies.token}`}})
+                    .then(resp => {console.log(resp.data[0])})
+                ])
+
+            }catch(err){
+                console.log(err)
+            }
+
+        }
+
+        fetchJob()
+
+    },[])
+
     return (
-        <div className="see-more-container postion-fixed top-50 ">
-            <h1>traki</h1>
+        <div className="see-more-container position-fixed bg-white">
         </div>
     )
 }
