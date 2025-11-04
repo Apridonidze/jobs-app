a4f72d7293fad356eff40bcc56e21472db5bf308
unknown <giorgiapridonidze08@gmail.com>
2025-11-04 13:21:43 +0400

    created fetching function in OtherUser.jsx to get data from users api

--- frontend/src/containers/OtherUser.jsx
+++ frontend/src/containers/OtherUser.jsx
@@ -1,16 +1,39 @@
+import axios from "axios"
+import { useEffect } from "react"
+import { useCookies } from "react-cookie"
 import { useParams } from "react-router-dom"
 
 const OtherUser = () => {
 
     const { userId } = useParams()
+    const {cookies} = useCookies(['token'])
+    const USER_URL = 
 
+    useEffect(() => {
 
-    console.log(userId)
+        const FetchUserData = async () => {
+
+            try{
+
+                await Promise.all([
+                    axios.get(`${USER_URL}/${userId}` , {headers : `${cookies.token}`})
+                    .then(resp => console.log(resp.data))
+                ])
+
+            }catch(err){
+                console.log(err)
+            }
+
+        }
+
+        FetchUserData()
+
+    },[])
 
     return (
         <div className="other-user-container">
             other user
-                  <p>User ID: {userId}</p>
+            <p>User ID: {userId}</p>
         </div>
     )
 }
