3c778104885d02f05fe0fb499e0f2dc6d2c0be17
unknown <giorgiapridonidze08@gmail.com>
2025-11-04 22:19:25 +0400

    fixed navbar footer buttons toggle based on user role

--- frontend/src/navbar/NavBarFooter.jsx
+++ frontend/src/navbar/NavBarFooter.jsx
@@ -3,19 +3,21 @@ const NavBarFooter =  ( { setFindJobs ,setCreateJobs , setToggleJobsListings,use
         <div className="nav-bar-footer d d-flex justify-content-between">
             
             <div className="footer-left">
-                {user && (user.role === 'Recruiter' ? <button className="btn" onClick={() => { setToggleApplied(false) ; setTogglePending(false) ; setCreateJobs(true) ; setFindJobs(false), setToggleJobsListings(false)}}>Create Job Offer</button> : <button className="btn" onClick={() => {setToggleSaved(true); setToggleApplied(false) ; setTogglePending(false) ; setCreateJobs(false) ; setFindJobs(true), setToggleJobsListings(false)}}>Find Job</button>)}
+                {user && (user.role === 'Recruiter' ?
+                     <button className="btn" onClick={() => { setToggleApplied(false) ; setTogglePending(false) ; setCreateJobs(true) ; setFindJobs(false), setToggleJobsListings(false)}}>Create Job Offer</button> 
+                     : <button className="btn" onClick={() => {setToggleSaved(false); setToggleApplied(false) ; setTogglePending(false) ; setCreateJobs(false) ; setFindJobs(true), setToggleJobsListings(false)}}>Find Job</button>)}
             
             </div>
             
             <div className="footer-right">
                 {user.role === 'Employee' && <>
-                    
-                    <button className="btn" onClick={() => {setToggleSaved(true); setToggleApplied(false) ; setTogglePending(false) ; setCreateJobs(false) ; setFindJobs(false), setToggleJobsListings(false)}}>Saved</button> 
-                    <button className="btn" onClick={() => {setToggleSaved(false); setToggleApplied(true) ; setTogglePending(false) ; setCreateJobs(false) ; setFindJobs(false), setToggleJobsListings(false)}}>Applied Jobs</button>
-                    <button className="btn" onClick={() => {setToggleSaved(false); setToggleApplied(false) ; setTogglePending(true) ; setCreateJobs(false) ; setFindJobs(false), setToggleJobsListings(false)}}>Job Pending</button>
-                    <button className="btn" onClick={() => {setToggleSaved(false); setToggleApplied(false) ; setTogglePending(false) ; setCreateJobs(false) ; setFindJobs(false), setToggleJobsListings(true)}}>Jobs Listings</button>
-
+                <button className="btn" onClick={() => {setToggleSaved(true); setToggleApplied(false) ; setTogglePending(false) ; setCreateJobs(false) ; setFindJobs(false), setToggleJobsListings(false)}}>Saved</button> 
+                <button className="btn" onClick={() => {setToggleSaved(false); setToggleApplied(true) ; setTogglePending(false) ; setCreateJobs(false) ; setFindJobs(false), setToggleJobsListings(false)}}>Applied Jobs</button>
+                {/*add button here to trigger jobs that we sent requestes and waiting for response */}
+                {user.role === 'Recruiter' && <button className="btn" onClick={() => {setToggleSaved(false); setToggleApplied(false) ; setTogglePending(true) ; setCreateJobs(false) ; setFindJobs(false), setToggleJobsListings(false)}}>Job Pending</button>}
                 </>}
+                <button className="btn" onClick={() => {setToggleSaved(false); setToggleApplied(false) ; setTogglePending(false) ; setCreateJobs(false) ; setFindJobs(false), setToggleJobsListings(true)}}>Jobs Listings</button>
+                
             </div>
 
         </div>
