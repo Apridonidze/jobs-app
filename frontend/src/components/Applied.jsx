cf196014d7831bd499f580599791b554d701deb3
unknown <giorgiapridonidze08@gmail.com>
2025-11-05 12:55:06 +0400

    created get statement in Applied.jsx that consumes data from Applied.js api

--- frontend/src/components/Applied.jsx
+++ frontend/src/components/Applied.jsx
@@ -1,4 +1,32 @@
+import { useEffect } from "react"
+import axios from 'axios'
+
+import { useCookies } from 'react-cookie'
+
 const Applied = () => {
+
+    const [cookies] = useCookies(['token'])
+    const MY_APPLIED_JOBS_URL = 'http://localhost:8080/applied/my-applied-jobs'
+
+    useEffect(() => {
+        const FetchUserJobs = async() => {
+
+            try{
+
+                await Promise.all([
+                    axios.get(MY_APPLIED_JOBS_URL , {headers : {Authorization : `Bearer ${cookies.token}`}})
+                    .then(resp => console.log(resp))
+                ])
+                
+            }catch(err){
+                console.log(err)
+            }
+
+        }
+
+        FetchUserJobs()
+    },[])
+
     return (
         <div className="applied-container">
             <h1>Your Applied Jobs:</h1>
