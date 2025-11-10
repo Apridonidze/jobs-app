562c084875e7c8609d575ad7356add27cea5c2a4
unknown <giorgiapridonidze08@gmail.com>
2025-11-10 15:40:25 +0400

    implemented same applied and saved buttons feature in Applied.jsx component

--- frontend/src/components/Applied.jsx
+++ frontend/src/components/Applied.jsx
@@ -1,12 +1,12 @@
 import JobHolder from "./JobHolder"
 
-const Applied = ( {user , jobs, handleSave , handleApply }) => {
+const Applied = ( {user , jobs, savedJobs ,appliedJobs }) => {
     
     return (
         <div className="applied-container">
             <h1>Your Applied Jobs:</h1>
             {jobs !== null && user && jobs.length > 0 ? jobs.map(job => 
-                <JobHolder job={job} user={user} handleSave={handleSave} handleApply={handleApply}/>
+                <JobHolder job={job} user={user} savedJobs={savedJobs} appliedJobs={appliedJobs}/>
             ) : <h1>No Jobs Applied Yet</h1>}
         </div>
     )
