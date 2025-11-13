62e70c20eea321e86cf1d4bdda791926b60383ed
unknown <giorgiapridonidze08@gmail.com>
2025-11-13 12:36:18 +0400

    added comments in MyJob.jsx to let recruiter know information about their job

--- frontend/src/createjobs/MyJob.jsx
+++ frontend/src/createjobs/MyJob.jsx
@@ -4,6 +4,9 @@ const MyJob = ( { job ,setToggleSeeMore} ) => {
         <div className="my-job-container" key={job.job_id}>
             <h3>{job.job_title}</h3>
             <h4>{job.job_desc}</h4>
+            <h4>applicants : 0</h4> {/* change 0 with applicant list count and highligth it*/}
+            <h4>date: </h4>{/* change date with job creation date*/}
+
             <button onClick={() => setToggleSeeMore({status:true , job_id : job.job_id})}>See More</button>
         </div>
     )
