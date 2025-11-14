efe05dc76a888fabbf6f2262a66a7c3e29c424c8
unknown <giorgiapridonidze08@gmail.com>
2025-11-14 17:17:45 +0400

    styled ProfileFinished Componentent positioning (did not make visible for phone user since is blocks screen much)

--- frontend/src/alerts/ProfileMessage.jsx
+++ frontend/src/alerts/ProfileMessage.jsx
@@ -1,10 +1,13 @@
+import '../main.css'; 
+
 const ProfileMessage = () => {
     return (
-        <div className="is-profile-finished-container position-fixed end-0 top-0 border border-1">
-            Profile is not finished
+        <div className="is-profile-finished-container border border-1 bg-white p-2 rounded-2">
+            <h5>Profile Is Not Finished!</h5>
+            <h6>Finish Your Profile To Explore Easier.</h6>
         </div>
-    )
-}
+    );
+};
 
 
-export default ProfileMessage
+export default ProfileMessage;
