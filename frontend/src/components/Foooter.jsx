a08f7d04187e5b251414ba386cf3eb90b5904fea
unknown <giorgiapridonidze08@gmail.com>
2025-11-14 17:11:50 +0400

    made Footer.jsx texts smaller so its takes less place

--- frontend/src/components/Foooter.jsx
+++ frontend/src/components/Foooter.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
             <div className="footer-start d-flex flex-column flex-sm-row gap-3">
 
                 <div className="about border-end px-3 pt-3 col text-start">
-                    <h5>About Jobs App : </h5>
+                    <h6>About Jobs App : </h6>
                     <div className="about-footer d-flex ">
                         <h6>Find Jobs, </h6>
                         <h6>Hire Fast.</h6>
@@ -15,23 +15,23 @@ const Footer = () => {
                 </div>
 
                 <div className="quick-links border-end px-3 pt-3 text-start col">
-                    <h5>Quick Links : </h5>
+                    <h6>Quick Links : </h6>
                     <div className="quick-links-footer d-flex flex-column gap-3">
-                        <Link to='/sign' className="fs-5">Sign</Link>
-                        <Link to='/login' className="fs-5">Login</Link>
+                        <Link to='/sign' className="fs-6">Sign</Link>
+                        <Link to='/login' className="fs-6">Login</Link>
                     </div>
                 </div>
 
                 <div className="contact border-end px-3 pt-3 text-start col">
 
                     <div className="contact-start d-flex flex-column">
-                        <h5>contact : </h5>
+                        <h6>contact : </h6>
                     </div>
 
                     <div className="contant-end d-flex flex-column text-start">
-                        <h5><i class="fa-solid fa-envelope"></i> Email : giorgiapridonidze08@gmail.com</h5>
-                        <h5><i class="fa-brands fa-github"></i> Github : <a href="https://github.com/Apridonidze">Github</a> </h5>
-                        <h5><i class="fa-brands fa-linkedin"></i> Linkedin : <a href="https://www.linkedin.com/in/giorgi-aphridonidze-7aa896291/">LinkedIn</a> </h5>
+                        <h6><i class="fa-solid fa-envelope"></i> Email : giorgiapridonidze08@gmail.com</h6>
+                        <h6><i class="fa-brands fa-github"></i> Github : <a href="https://github.com/Apridonidze">Github</a> </h6>
+                        <h6><i class="fa-brands fa-linkedin"></i> Linkedin : <a href="https://www.linkedin.com/in/giorgi-aphridonidze-7aa896291/">LinkedIn</a> </h6>
                     </div>
                     
 
@@ -39,8 +39,8 @@ const Footer = () => {
 
                 
                 <div className="copy-rights px-3 py-3 text-start col">
-                    <h5>Jobs App © 2025</h5>
-                    <h5>Built by Giorgi Apridonidze.</h5>
+                    <h6>Jobs App © 2025</h6>
+                    <h6>Built by Giorgi Apridonidze.</h6>
                 </div>
                 
 
