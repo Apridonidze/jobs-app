af68c5367f9d6aac4d5d7d09ff501ee236d26740
unknown <giorgiapridonidze08@gmail.com>
2025-11-14 17:31:24 +0400

    styled and documented NavBarHeader.jsx

--- frontend/src/navbar/NavBarHeader.jsx
+++ frontend/src/navbar/NavBarHeader.jsx
@@ -1,13 +1,23 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom"; //importing react library for navigation
 
 const NavBarHeader = ( { user } ) => {
     return(
-        <div className="nav-bar-header d d-flex justify-content-between text-start">
-            <div className="col w-25"><Link to='/'>LOGO</Link></div> 
-            <div className="col w-25 text-end"><Link to='/my-account'>{user && user.name}</Link></div>
+        <div className="nav-bar-header d d-flex justify-content-between text-start py-2">
+            
+            <div className="header-start">
+
+                <Link to='/authentication' className="alert-link fs-4">JOBS APP</Link>
+
+            </div>
+
+            <div className="header-end">
+
+                <Link to='/my-account' className="text-dark link-underline  link-underline-opacity-0">{user && user.name}</Link>
+
+            </div>
+        
         </div>
-    )
-}
+    );
+};
 
-
-export default NavBarHeader
+export default NavBarHeader;//exporting component
