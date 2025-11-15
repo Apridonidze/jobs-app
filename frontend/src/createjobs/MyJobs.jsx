2f020987c0246d92fbcd6eac4b80fc594682c6fc
unknown <giorgiapridonidze08@gmail.com>
2025-11-15 16:04:55 +0400

    documentated and cleaned uip MyJobs.jsx component

--- frontend/src/createjobs/MyJobs.jsx
+++ frontend/src/createjobs/MyJobs.jsx
@@ -1,4 +1,4 @@
-import MyJob from "./MyJob"
+import MyJob from "./MyJob"; //importing react component
 
 const MyJobs = ( { yourJobs , setToggleSeeMore } ) => {
 
@@ -6,10 +6,11 @@ const MyJobs = ( { yourJobs , setToggleSeeMore } ) => {
         <div className="my-jobs-container d-flex flex-column">
 
             <h1>Jobs Created By You: </h1>
-            
+
             {yourJobs ? yourJobs.reverse().map(job => <MyJob job={job} setToggleSeeMore={setToggleSeeMore}/>) :  <h1>No Jobs Yet.</h1> }
                        
         </div>
-)}
+    );
+}; //component checks if jobs are already fetched if so it returns No Jobs Yet text if user has not created jobs yet else it returns reversed jobs lists (first one is latest created job) in MyJob components
 
-export default MyJobs
+export default MyJobs; //exporting component
