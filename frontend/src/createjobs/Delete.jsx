863fd9a876dde315f7a6bd516fa852d0723abd75
unknown <giorgiapridonidze08@gmail.com>
2025-11-15 17:39:54 +0400

    cleaned up and documentated Delete.jsx

--- frontend/src/createjobs/Delete.jsx
+++ frontend/src/createjobs/Delete.jsx
@@ -1,24 +1,26 @@
-import axios from "axios"
-import { useCookies } from "react-cookie"
-import '../main.css'
+import axios from "axios";
+import { useCookies } from "react-cookie";//importing react libraries
+
+import '../main.css';//importing css
+
 const Delete = ( { toggleDelete , setToggleDelete } ) => {
 
-    const [cookies] = useCookies(['token'])
-    const DELETE_JOB_URL = 'http://localhost:8080/jobs/delete-job';
+    const [cookies] = useCookies(['token']); //cookies
+    const DELETE_JOB_URL = 'http://localhost:8080/jobs/delete-job'; //api for deleting job
 
     const handleDeleteJob = async () => {
         
         try{
 
             await Promise.all([
-                axios.delete(`${DELETE_JOB_URL}/${toggleDelete.job_id}` , {headers : {Authorization : `Bearer ${cookies.token}`}} )
-                .then(resp => {console.log(resp), window.location.reload()})
-            ])
+                axios.delete(`${DELETE_JOB_URL}/${toggleDelete.job_id}` , {headers : {Authorization : `Bearer ${cookies.token}`}} )//sends delete statement to server
+                .then(resp => {console.log(resp), window.location.reload()}) //consoles success request and realods page so it will be updated
+            ]);
 
         }catch(err){
-            console.log(err)
-        }
-    }
+            console.log(err);//consoles erorr in console
+        };
+    };
 
     return (
         <div className="delete-container position-fixed bg-white text-center p-3 rounded-2 " >
@@ -28,8 +30,8 @@ const Delete = ( { toggleDelete , setToggleDelete } ) => {
                 <button className="btn border btn-md" onClick={() => setToggleDelete({status: false , job_id : false})}>No</button>
             </div>
         </div>
-    )
-}
+    );
+};
 
 
-export default Delete
+export default Delete; //exporting component
