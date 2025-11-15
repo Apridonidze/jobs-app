a125032aa57f8dd4f4d008816ca52ea9dbdf50f1
unknown <giorgiapridonidze08@gmail.com>
2025-11-15 16:02:32 +0400

    cleaned up and documentated CreateJobsInput.jsx component , implemented Error component when recrutier makes request to server but there is internal error

--- frontend/src/createjobs/CreateJobsInput.jsx
+++ frontend/src/createjobs/CreateJobsInput.jsx
@@ -1,130 +1,128 @@
-import axios from "axios"
-import { useEffect, useRef, useState } from "react"
-import { useCookies } from "react-cookie"
+import axios from "axios" ;
+import { useCookies } from "react-cookie";//imporrting react libraries
 
-import '../main.css';
+import { useRef, useState } from "react"; //importing react hooks
 
-const CreateJobsInput = ( {  setIsJobsSuccessful ,setToggleJobsMessage , setJobsMessage,SubmitBtnRef } ) => {
+import '../main.css'; //importing css 
+
+const CreateJobsInput = ( {  setIsJobsSuccessful ,setToggleJobsMessage , setJobsMessage,SubmitBtnRef ,setToggleError} ) => {
 
 
 
-    const [cookies,setCookies,removeCookies] = useCookies(['token'])
-    
-    const NEW_JOBS_URL = 'http://localhost:8080/jobs/new-jobs'
+    const [cookies] = useCookies(['token']); //cookies
+    const NEW_JOBS_URL = 'http://localhost:8080/jobs/new-jobs'; //post url to create new job
 
-    const [seconds,setSeconds] = useState(3)
 
-    const [title,setTitle] = useState('')
-    const [desc, setDesc] = useState('')
-    const [employeeList,setEmployeeList] = useState([])
-    const [technologies, setTechnologies] = useState([])
-    const [languages, setLanguages] = useState([])
+    const [title,setTitle] = useState('');
+    const [desc, setDesc] = useState('');
+    const [employeeList,setEmployeeList] = useState([]);
+    const [technologies, setTechnologies] = useState([]);
+    const [languages, setLanguages] = useState([]); //states for inputs
 
-    const [titleErr, setTitleErr] = useState('')
-    const [descErr, setDescErr] = useState('')
-    const [employeeListErr, setEmployeeListErr] = useState('')
-    const [technologiesErr, setTechnologiesErr] = useState('')
-    const [languagesErr, setLanguagesErr] = useState('')
+    const [titleErr, setTitleErr] = useState('');
+    const [descErr, setDescErr] = useState('');
+    const [employeeListErr, setEmployeeListErr] = useState('');
+    const [technologiesErr, setTechnologiesErr] = useState('');
+    const [languagesErr, setLanguagesErr] = useState('');//states for input errors
 
-    const titleRef = useRef(null)
-    const descRef = useRef(null)
-    const employeeListRef = useRef(null)
-    const technologiesRef = useRef(null)
-    const languageRef = useRef(null)
+    const titleRef = useRef(null);
+    const descRef = useRef(null);
+    const employeeListRef = useRef(null);
+    const technologiesRef = useRef(null);
+    const languageRef = useRef(null);//refs to style inputs error || success
+
 
     const handleEmployees = (e) => {
 
-        if(e === 'blank')return
+        if(e === 'blank') return; //retruns nothing if default value is choosed
         
-        if(employeeList.includes(e))return
+        if(employeeList.includes(e))return; //returns nothing if employee role is already in list
 
-        setEmployeeList(employeeList => [...employeeList , e])
+        setEmployeeList(employeeList => [...employeeList , e]); //else sets role in state
         
-    }
+    };//handles employee's roles selection input
 
     const handleTechnologies = (e) => {
 
         
-        if(e === 'blank')return
+        if(e === 'blank')return; //retruns nothing if default value is choosed
         
-        if(technologies.includes(e))return
+        if(technologies.includes(e))return; //retruns nothing if employee technologiy is already in list
 
-        setTechnologies(technologies => [...technologies, e])
+        setTechnologies(technologies => [...technologies, e]); //else sets technology in state
 
-    }
+    };//handles employee's technologies selection input
 
     const handleLanguages = (e) => {
 
         
-        if(e === 'blank')return
+        if(e === 'blank')return; //returns nothing if default value is choosed
         
-        if(languages.includes(e))return
+        if(languages.includes(e))return; //returns nothing if employee language is already in list
 
-        setLanguages(languages => [...languages, e])
+        setLanguages(languages => [...languages, e]); //else sets language in state
         
-    }
+    }; //handles employee's speaking language selection input
 
 
     const handleSubmit = async (e) => {
 
-        e.preventDefault();
+        e.preventDefault(); //prevent page refresh when clicked
 
         
-        let data = {}
-        let isValid = false
+        let data = {};
+        let isValid = false; //variables for inputs
 
 
         if(title == '' || title == ' ' || title == null ||title == undefined){isValid = false ; setTitleErr(`This Field Can't Be Empty`) ; titleRef.current.classList.add('is-invalid');titleRef.current.classList.remove('is-valid')}
         else if(title.length < 6){isValid = false ; setTitleErr(`Title Length Is Too Small`) ; titleRef.current.classList.add('is-invalid');titleRef.current.classList.remove('is-valid')}
         else if(title.length > 55){isValid = false ; setTitleErr(`Title Length Is Too Long`) ; titleRef.current.classList.add('is-invalid');titleRef.current.classList.remove('is-valid')}
-        else {isValid = true ; setTitleErr('') ; titleRef.current.classList.remove('is-invalid'); ;titleRef.current.classList.add('is-valid'); data = {...data, title: title}}
+        else {isValid = true ; setTitleErr('') ; titleRef.current.classList.remove('is-invalid'); ;titleRef.current.classList.add('is-valid'); data = {...data, title: title}} //filters recruiterse title input and validates it
 
         if(desc == '' || desc == ' ' || desc == null ||desc == undefined){isValid = false ; setDescErr(`This Field Can't Be Empty`) ; descRef.current.classList.add('is-invalid');descRef.current.classList.remove('is-valid')}
         else if(desc.length < 25){isValid = false ; setDescErr(`Description Length Is Too Small`) ; descRef.current.classList.add('is-invalid');descRef.current.classList.remove('is-valid')}
         else if(desc.length > 500){isValid = false ; setDescErr(`Description Length Is Too Long`) ; descRef.current.classList.add('is-invalid');descRef.current.classList.remove('is-valid')}
-        else {isValid = true ; setTitleErr('') ; descRef.current.classList.remove('is-invalid'); ;descRef.current.classList.add('is-valid'); data = {...data, desc: desc}}
+        else {isValid = true ; setTitleErr('') ; descRef.current.classList.remove('is-invalid'); ;descRef.current.classList.add('is-valid'); data = {...data, desc: desc}}//filters recruiterse description input and validates it
 
         
         if(employeeList == '' || employeeList== ' ' || employeeList == null ||employeeList == undefined || employeeList.length < 1){isValid = false ; setEmployeeListErr(`This Field Can't Be Empty`) ; employeeListRef.current.classList.add('is-invalid');employeeListRef.current.classList.remove('is-valid')}
         else if(employeeList.length > 10){isValid = false ; setEmployeeListErr(`Invalid Employees Amount`) ; employeeListRef.current.classList.add('is-invalid');employeeListRef.current.classList.remove('is-valid')}
-        else {isValid = true ; setEmployeeListErr('') ; employeeListRef.current.classList.remove('is-invalid'); ;employeeListRef.current.classList.add('is-valid'); data = {...data, employeeList: employeeList}}
+        else {isValid = true ; setEmployeeListErr('') ; employeeListRef.current.classList.remove('is-invalid'); ;employeeListRef.current.classList.add('is-valid'); data = {...data, employeeList: employeeList}} //filters recruiterse employeelist input and validates it
 
           
         if(technologies== '' || technologies== ' ' || technologies == null ||technologies == undefined || technologies.length < 1){isValid = false ; setTechnologiesErr(`This Field Can't Be Empty`) ; technologiesRef.current.classList.add('is-invalid');technologiesRef.current.classList.remove('is-valid')}
         else if(technologies.length > 26){isValid = false ; setTechnologiesErr(`Invalid Technologies Amount`) ; technologiesRef.current.classList.add('is-invalid');technologiesRef.current.classList.remove('is-valid')}
-        else {isValid = true ; setTechnologiesErr('') ; technologiesRef.current.classList.remove('is-invalid'); ;technologiesRef.current.classList.add('is-valid'); data = {...data, technologies: technologies}}
+        else {isValid = true ; setTechnologiesErr('') ; technologiesRef.current.classList.remove('is-invalid'); ;technologiesRef.current.classList.add('is-valid'); data = {...data, technologies: technologies}} //filters recruiterse technologies input and validates it
 
         if(languages == '' || languages== ' ' || languages == null ||languages == undefined || languages.length < 1){isValid = false ; setLanguagesErr(`This Field Can't Be Empty`) ; languageRef.current.classList.add('is-invalid');languageRef.current.classList.remove('is-valid')}
         else if(languages.length > 3){isValid = false ; setLanguagesErr(`Invalid Technologies Amount`) ; languageRef.current.classList.add('is-invalid');languageRef.current.classList.remove('is-valid')}
-        else {isValid = true ; setLanguagesErr('') ; languageRef.current.classList.remove('is-invalid'); ;languageRef.current.classList.add('is-valid'); data = {...data, languages: languages}}
+        else {isValid = true ; setLanguagesErr('') ; languageRef.current.classList.remove('is-invalid'); ;languageRef.current.classList.add('is-valid'); data = {...data, languages: languages}}//filters recruiterse language input and validates it
 
-        if(isValid){
+
+        if(isValid){ //if all inputs are valid then this function executesw
             try{
-            axios
-            .post(NEW_JOBS_URL, {data}  , {headers : {Authorization : `Bearer ${cookies.token}`}})
-            .then(resp => {
-                setJobsMessage(resp.data.message)
-                setIsJobsSuccessful(true)
-                setToggleJobsMessage(true)
-                console.log(resp)
-            })
-
-
+                await axios
+                .post(NEW_JOBS_URL, {data}  , {headers : {Authorization : `Bearer ${cookies.token}`}})
+                .then(resp => {
+                    setJobsMessage(resp.data.message); //sets jobs creation message 
+                    setIsJobsSuccessful(true); //sets jobs creattion success to true
+                    setToggleJobsMessage(true); //toggles jobs message component
+                    setToggleError(false) ; //does not toggle error message component since job is created successfully
+            });
             }catch(err){
-                setJobsMessage(err.data.err)
-                setIsJobsSuccessful(false)
-                setToggleJobsMessage(true)
-                console.log(err)
-            }
+                setToggleError(true) //toggles error mesasge component
+                console.log(err); //consoles error message
+            };
 
 
-        }
+        };
 
-    }
+    }; //function toggles when recruiter clicks on Create Job Offer button , it validates inputs and returns errors , if all inputs are correct then it executes post statement to server to insert new job variables into database
 
 
     return (
         <div className="create-jobs-input-container container position-fixed bg-white d-flex flex-column top-25 w-50 d-sm-w-50 p-4 rounded-2" >
+
 
             <h1>Create Job Offer</h1>
             
@@ -234,8 +232,8 @@ const CreateJobsInput = ( {  setIsJobsSuccessful ,setToggleJobsMessage , setJobs
             </form>
            
         </div>
-    )
-}
+    );
+};
 
 
-export default CreateJobsInput
+export default CreateJobsInput; //exporting component
