32783db9bb7504a4ca1f54eeb669b6491201b072
unknown <giorgiapridonidze08@gmail.com>
2025-11-15 17:47:13 +0400

    styled saved.jsx

--- frontend/src/components/Saved.jsx
+++ frontend/src/components/Saved.jsx
@@ -3,12 +3,19 @@ import JobHolder from './JobHolder'; //importing react component
 const Saved = ( { user , jobs, savedJobs, appliedJobs } ) => {
 
     return (
-        <div className="saved-container">
-            <h1>Your Saved Jobs:</h1>
+        <div className="saved-container container d-flex flex-column mt-5 min-vh-100">
+            <div className="saved-header mb-3">
             
-           {jobs && user && jobs.length > 0 ? jobs.map(job => 
-                <JobHolder job={job} user={user} savedJobs={savedJobs} appliedJobs={appliedJobs}/>
-            ) : <h1>No Jobs Applied Yet</h1>}
+                <h1>Your Saved Jobs:</h1>
+            
+            </div>
+            
+            <div className="saved-body">
+                
+                {jobs && user && jobs.length > 0 ? jobs.map(job => 
+                    <JobHolder job={job} user={user} savedJobs={savedJobs} appliedJobs={appliedJobs}/>
+                ) : <h1>No Jobs Applied Yet</h1>}
+            </div>
         </div>
     );
 }; //component checks if jobs are fetched from server and then displayes saved jobs if employee has any else it returns no jobs applied
