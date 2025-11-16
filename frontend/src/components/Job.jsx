768af021b0e8f8170f149be73edd98d9ba2c0001
unknown <giorgiapridonidze08@gmail.com>
2025-11-16 13:40:30 +0400

    cleaned up documenatated and styled Job.jsx

--- frontend/src/components/Job.jsx
+++ frontend/src/components/Job.jsx
@@ -1,67 +1,87 @@
-import axios from "axios"
-import {  useCookies } from "react-cookie"
+import axios from "axios";
+import { useCookies } from "react-cookie"; //importing react libraries
 
-import {  useEffect, useState } from "react"
+import { useEffect, useState } from "react"; //importing react hooks
 
+const Job = ({ job, user, setToggleSeeMore,savedJobs, appliedJobs , setToggleError}) => {
 
-const Job = ({ job, user, setToggleSeeMore,savedJobs, appliedJobs  }) => {
-
-    const [cookies] = useCookies(['token'])
-    const [isApplied, setIsApplied] = useState(null)
-    const [isSaved, setIsSaved] = useState(null)
-
+    const [cookies] = useCookies(['token']); //cookies
+    const [isApplied, setIsApplied] = useState(null);//state for employee to see if they already applied for job
+    const [isSaved, setIsSaved] = useState(null);//state for employee to see if they already saved for job
     
-    const SAVE_URL = 'http://localhost:8080/saved'
-    const APPLY_URL = 'http://localhost:8080/applied'
+    const SAVE_URL = 'http://localhost:8080/saved'; //api for saved jobs
+    const APPLY_URL = 'http://localhost:8080/applied'; //api for applied
 
 
     const handleSave = async() => {
-        const res = await axios.post(`${SAVE_URL}/${job.job_id}` , {} , {headers: {Authorization : `Bearer ${cookies.token}`}})
-        console.log(res)
-        setIsSaved(true)
-    }
 
+        try{
+
+            await axios.post(`${SAVE_URL}/${job.job_id}` , {} , {headers: {Authorization : `Bearer ${cookies.token}`}}).then(setIsSaved(true), setToggleError(false)) //sends post request to server to save job in db
+        
+        }catch(err){
+
+            console.log(err); //consoles error in console
+            setToggleError(true); //toggles Error component if error occurs
+
+        };
+        
+    };
     
     const handleApply = async() => {
         
-        const res = await axios.post(`${APPLY_URL}/${job.job_id}` , {} , {headers: {Authorization : `Bearer ${cookies.token}`}})
-        console.log(res)
-        setIsApplied(true)
-    }
+        try{
+        
+            await axios.post(`${APPLY_URL}/${job.job_id}` , {} , {headers: {Authorization : `Bearer ${cookies.token}`}}).then(setIsApplied(true) ,setToggleError(false))//sends post request to server to apply for job and save in database
+        
+        }catch(err){
+            
+            console.log(err); //consoles error in console
+            setToggleError(true); //toggles Error component if error occurs
 
+        };
+        
+    };
 
 
      useEffect(() => {
         
         const filterSavedJob = async() =>{
-            const savedJobList = await savedJobs.filter(savedJob => savedJob.job_id == job.job_id)
-        if(savedJobList.length > 0){
 
-            if(savedJobList[0].job_id === job.job_id) setIsSaved(true)
-        }
-        return 
-        }
+            const savedJobList = await savedJobs.filter(savedJob => savedJob.job_id == job.job_id);//fetches and filters job based on jobid and checks if it is already saved
+
+            if(savedJobList.length > 0){//checks if job is saved
+
+                if(savedJobList[0].job_id === job.job_id) setIsSaved(true); //sets state to true if job is saved and styles button if so
+
+            };
+            
+            return; //if job is not saved returns nothing
+        };
 
         const filterAppliedJob = async() =>{
-            const appliedJobList = await appliedJobs.filter(appliedJob => appliedJob.job_id == job.job_id)
-        if(appliedJobList.length > 0){
 
-            if(appliedJobList[0].job_id === job.job_id) setIsApplied(true)
-                else return
-        }
+            const appliedJobList = await appliedJobs.filter(appliedJob => appliedJob.job_id == job.job_id); //fetches and filters job based on jobid and checks if it is already applied
+
+            if(appliedJobList.length > 0){
+
+                if(appliedJobList[0].job_id === job.job_id) setIsApplied(true); //sets state t otrue if job is applied and styled button if so
+                
+            };
+
+            return; //if job is not applied retursn nothing
         } 
 
-        filterSavedJob()
-        filterAppliedJob()
+        filterSavedJob(); 
+        filterAppliedJob();//declaring functions
 
-     },[savedJobs,appliedJobs])
+     },[savedJobs,appliedJobs]) //function run when this variables are changed
 
 
 
     return (
-        <div className="job-container border d-flex flex-column justify-content-between p-1 " key={job.job_id}>
+        <div className="job-container  border rounded-2 d-flex flex-column justify-content-between p-3 " key={job.job_id}>
             
-
 
                 <div className="job-header">
                     <h4 className="text-break">{job.job_title}</h4>
@@ -95,9 +115,8 @@ const Job = ({ job, user, setToggleSeeMore,savedJobs, appliedJobs  }) => {
 
                 </div>
 
-
         </div>
-    )
-}
+    );
+};//component renders job data and apply/save button , if user already applied/saved job it returns saved/applied button
 
-export default Job
+export default Job; //exporting component
--- frontend/src/components/JobListings.jsx
+++ frontend/src/components/JobListings.jsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
+import Error from "../alerts/Error";
 import Job from "./Job";//importing react component
 
 const JobListings = ( { jobs, user , setToggleSeeMore, savedJobs, appliedJobs } ) => {
 
+    const [toggleError,setToggleError] = useState(false)
+
     return (
-        <div className="job-listing-container container my-3 ">
+        <div className="job-listing-container container my-3 d-flex flex-column gap-2">
           
 
             <div className="job-listing-header">
@@ -15,11 +19,14 @@ const JobListings = ( { jobs, user , setToggleSeeMore, savedJobs, appliedJobs } 
              <div className="job-listing-body row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
                 
                 {jobs.length < 1 ? <h1>No Jobs Posted Yet.</h1> : jobs.slice().reverse().map((job) => (
-                    <Job job={job} user={user} setToggleSeeMore={setToggleSeeMore} savedJobs={savedJobs} appliedJobs={appliedJobs}/>
+                    <Job job={job} user={user} setToggleSeeMore={setToggleSeeMore} savedJobs={savedJobs} appliedJobs={appliedJobs} setToggleError={setToggleError}/>
                 ))}
 
 
             </div>
+
+            {toggleError && <Error setToggleError={setToggleError} />}
+
         </div>
     );
 };//component returns all jobs from database . if there is no jobs it returns No Jobs Posted Yet text
