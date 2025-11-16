e5fa349aa4d10e2f33d4a23467fe792f5e076e4b
unknown <giorgiapridonidze08@gmail.com>
2025-11-16 19:18:30 +0400

    styled EmployeeForms.jsx and documenateted it

--- frontend/src/components/EmployeeForms.jsx
+++ frontend/src/components/EmployeeForms.jsx
@@ -1,33 +1,37 @@
 const EmployeeForms = ( { setToggleUploadTechnologies , technologies,  roles ,setToggleUploadRole  } ) => {
     return(
 
-        <div className="employee-forms-container d-flex flex-column gap-3 ">
+        <div className="employee-forms-container d-flex flex-column gap-3 w-50">
 
             <div className="role-container">
 
                 <span className="fs-5">I Am a :</span>
 
+                <div className="d-flex flex-wrap">
                     {roles && roles.map((role , roleId) => (
-                    <span className="bg-success text-white fs-6 p-1 rounded-2 m-2 col w-100" key={roleId}>{role}</span>
+                    <h6 className="bg-success text-white m-1  p-2 rounded-2 " key={roleId} >{role}</h6>
                 ))}
             
                 
-                <span className="bg-primary text-white rounded-2 p-1 fs-6" onClick={() => setToggleUploadRole(true)}>{roles && roles.length < 1 ? 'Add Role...' : 'Edit...'}</span>
+                <span className="bg-primary text-white rounded-2 p-1 fs-6 m-1"  onClick={() => setToggleUploadRole(true)}>{roles && roles.length < 1 ? 'Add Role...' : 'Edit...'}</span>
 
 
+                </div>
 
             </div>
 
             <div className="technologies-container">
                 
-                <span>Your Technologies:</span>
+                <span className="fs-5">Your Technologies:</span>
             
-                {technologies && technologies.map((technology , technologyId) => (
-                    <span className="bg-success text-white" key={technologyId}>{technology}</span>
+                <div className="d-flex flex-wrap justify-content-start align-items-center">
+                    {technologies && technologies.map((technology , technologyId) => (
+                    <span className="bg-success text-white m-1  p-2 rounded-2 " style={{height : '4vh'}} key={technologyId}>{technology}</span>
                 ))}
             
-                <span className="bg-primary text-white" onClick={() => setToggleUploadTechnologies(true)}>{technologies && technologies.length === 0 ? 'Add Technologies...' : 'Edit...'}</span>
+                <span className="bg-primary text-white rounded-2 p-1 fs-6 m-1" onClick={() => setToggleUploadTechnologies(true)}>{technologies && technologies.length === 0 ? 'Add Technologies...' : 'Edit...'}</span>
             
+                </div>
             </div>
             
 
