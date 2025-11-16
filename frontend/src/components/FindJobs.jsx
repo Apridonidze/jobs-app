002167b886806f09778425caeca9da6f9f4777cc
unknown <giorgiapridonidze08@gmail.com>
2025-11-16 13:42:30 +0400

    cleaned up documentated and styled FindJobs.jsx

--- frontend/src/components/FindJobs.jsx
+++ frontend/src/components/FindJobs.jsx
@@ -1,14 +1,14 @@
-import FindJobsMessage from "../alerts/FindJobsMessage"
-import FilteredJobs from "./FilteredJobs"
+import FindJobsMessage from "../alerts/FindJobsMessage";
+import FilteredJobs from "./FilteredJobs"; //importing components
+
 const FindJobs = ( { isProfileFinished, jobs } ) => {
         
     return(
-        <div className="create-jobs-container ">
+        <div className="create-jobs-container container">
             {isProfileFinished && !isProfileFinished ? <FindJobsMessage /> : <FilteredJobs jobs={jobs} />} 
 
         </div>
-    )
-}
+    );
+};//checks if user profile is finished and if so it returns filtered jobs that are filtered based on users technologies , language and role else it returns message that user has to finish their profile to generate jobs based on their preferences
 
-
-export default FindJobs
+export default FindJobs;//exporting component
