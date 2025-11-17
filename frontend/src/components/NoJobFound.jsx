4e7f3bf0db9290fc514367cef2f539e73ea8c7d5
unknown <giorgiapridonidze08@gmail.com>
2025-11-17 10:44:21 +0400

    modified texty in NoJobFound.jsx and documentated it

--- frontend/src/components/NoJobFound.jsx
+++ frontend/src/components/NoJobFound.jsx
@@ -1,12 +1,11 @@
 const NoJobFound = () => {
     return (
         <div className="no-job-found w-100">
-            <h1>No Jobs Found For You</h1>
-            <h2>Wait Untill Somebody Will Post Job That Matches Your Profile</h2>
-            <h3>See All Jobs In Jobs Listings</h3>
+            <h1>No Jobs Found That Matches Your Profile</h1>
+            <h2>Wait Untill Somebody Posts Job That Your Profile Matches </h2>
+            <h3>See All Jobs In Jobs Listings.</h3>
         </div>
-    )
-}
-//add link to you profile to finish profile
+    );
+};
 
-export default NoJobFound
+export default NoJobFound; //exporting component
