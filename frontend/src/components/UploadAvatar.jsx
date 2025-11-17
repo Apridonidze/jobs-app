70823441a07408967fc650d7c35b477232239531
unknown <giorgiapridonidze08@gmail.com>
2025-11-17 11:21:40 +0400

    styled UploadAvatar.jsx for responsive design and documentated it

--- frontend/src/components/UploadAvatar.jsx
+++ frontend/src/components/UploadAvatar.jsx
@@ -1,26 +1,28 @@
+import '../main.css' ; //importing css file
+
 const UploadAvatar = ( { handleProfileSend,avatarImg,DefaultImage } ) => {
 
-   
-
     return(
-        <div className="upload-avatar-container container position-fixed w-50 bg-white d-flex flex-column justify-content-center text-center align-items-center">
-            
-            
-        <img src={avatarImg || DefaultImage} className="border border-rounded" style={{borderRadius : '100%', width: '350px' , height:'350px'}}/>
-
-        <span>Upload Your Avatar</span>    
         
-        <div className="form-floating">
+        <div className="upload-avatar-container container position-fixed bg-white p-3 rounded-2 gap-3 col-12 col-sm-6"  >
             
-            <input type="file" className="form-control" onChange={(e) => handleProfileSend(e)}  accept="image/png, image/jpeg, image/webp" />
-            <label htmlFor="">Add Your Profile Picture...</label>
-    
-        </div>
+            <div className="form-header d-flex flex-column text-center gap-3">
+                
+                <img src={avatarImg || DefaultImage} className="border border-rounded mx-auto" style={{borderRadius : '100%' , width : "30vh", height:'30vh' , maxWidth: '300px' , maxHeight: '300px'}}/>
+
+                <span>Upload Your Avatar</span>    
+            
+            </div>
+            
+            <div className="form-floating">
+                
+                <input type="file" className="form-control" onChange={(e) => handleProfileSend(e)}  accept="image/png, image/jpeg, image/webp" />
+                <label htmlFor="">Add Your Profile Picture...</label>
+        
+            </div>
     
     </div>
-    
-)
-}
+    );
+};
 
-
-export default UploadAvatar
+export default UploadAvatar; //exporting component
