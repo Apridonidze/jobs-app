086926d637c088835ee176e082f7b43fd72c2b53
unknown <giorgiapridonidze08@gmail.com>
2025-11-18 15:16:57 +0400

    documentated and Cleanedup FindJobsMessage.jsx

--- frontend/src/alerts/FindJobsMessage.jsx
+++ frontend/src/alerts/FindJobsMessage.jsx
@@ -5,8 +5,8 @@ const FindJobsMessage = () => {
             <h2>Finish Your Profile To Generate Jobs That Are Related To You</h2>
             <h3>See Unfiltered Jobs in Jobs Listing Page...</h3>
         </div>
-    )
-}
+    );
+};
 
 
-export default FindJobsMessage
+export default FindJobsMessage; //exporting component
