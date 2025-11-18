28bc674d97c5abad84d57de5d0b71fb015a16ecc
unknown <giorgiapridonidze08@gmail.com>
2025-11-18 17:43:14 +0400

    cleaned up and documentated App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import { Navigate, RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { useCookies } from 'react-cookie'
+import { useCookies } from 'react-cookie';
+import { Navigate, RouterProvider, createBrowserRouter } from 'react-router-dom'; //importing react libraries
 
 import Authentication from './containers/Authentication';
 import Login from './containers/Login';
@@ -7,25 +7,23 @@ import Sign from './containers/Sign';
 import Main from './containers/Main';
 import NotFound from './containers/NotFound';
 import MyUser from './myuser/MyUser';
-import OtherUser from './containers/OtherUser';
+import OtherUser from './containers/OtherUser'; //importing react container components
 
 const App = () => {
   
-  const [ cookies ] = useCookies(['token'])
-  const hasToken = cookies.token && cookies.token !== "undefined";
+  const [ cookies ] = useCookies(['token']) ;//cookies
+  const hasToken = cookies.token && cookies.token !== "undefined"; //checks if user has cookies
 
   const router = createBrowserRouter([
     
-    {path: '/', element : hasToken ? <Main /> : <Navigate to='/authentication' />  },//directs user to authentication page when not loggined
-    {path: '/authentication', element : <Authentication /> }, //directs user to main page when loggined
-    {path : '/sign', element :<Sign />}, //check if user has cookies and make them direct to main page if so (do not change now)
-    {path : '/login', element : <Login />},//check if user has cookies and make them direct to main page if so (do not change now)
-    {path : '/my-account', element : hasToken ? <MyUser /> : <Navigate to='/authentication' />}, //if user doesnot have cookies do not redirect them in this page (add in future)
-    {path : '/user-account/:userId', element : hasToken ? <OtherUser /> : <Navigate to='/authentication' />},
-    {path : '*', element : <NotFound />}
-  ])
-  
-  
+    {path: '/', element : hasToken ? <Main /> : <Navigate to='/authentication' />  },//directs user main page if he has cookies , else component navigates user to authentication page(cookies required)
+    {path: '/authentication', element : <Authentication /> }, //directs user to authentication component (cookies not required)
+    {path : '/sign', element :<Sign />}, //directs user to sign page (cookies not requiured)
+    {path : '/login', element : <Login />},//directs user to login page (cookies not requiured)
+    {path : '/my-account', element : hasToken ? <MyUser /> : <Navigate to='/authentication' />}, //directs user to their user page , if they do not have cookies then component navigates user to authentication page
+    {path : '/user-account/:userId', element : hasToken ? <OtherUser /> : <Navigate to='/authentication' />}, //directs user to other users page , if they do not have cookies then component navigates user to authentication page
+    {path : '*', element : <NotFound />}//directs user to not found page component when they visit invalid page
+  ]);//routes 
   
   return (
     
@@ -36,7 +34,6 @@ const App = () => {
     </div>
   
   );
-};
+};//implementing routerprovider to provide user with routes 
 
-
-export default App;
+export default App; //exporting component
