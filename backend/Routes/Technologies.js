6a0622968e1dea26937afcdc5597222087243ad3
unknown <giorgiapridonidze08@gmail.com>
2025-11-19 03:52:46 +0400

    documentated Technologies.js

--- backend/Routes/Technologies.js
+++ backend/Routes/Technologies.js
@@ -1,66 +1,58 @@
-const express = require('express')
-const TechRouter = express.Router()
+const express = require('express'); //importing express
+const TechRouter = express.Router(); //creating route
 
-const verifyToken = require('../config/verifyToken')
-const rateLimiter = require('../config/rateLimiter')
+const db = require('../db/db');
+const verifyToken = require('../config/verifyToken');
+const rateLimiter = require('../config/rateLimiter');//importing middlewares
 
-const TechnologiesSchema = require('../schemas/TechnologiesSchema')
-const db = require('../db/db')
+const TechnologiesSchema = require('../schemas/TechnologiesSchema'); //impporting schemas
 
 TechRouter.get('/user-technologies', verifyToken ,async (req,res) => {
 
-    const [ rows ] = await db.query('select * from user_technologies where user_id = ?', [req.user.userId])
-
+    
     try{
 
-        if(rows.length < 1){
-            return res.status(204).json('No Technologies Yet')
-        }
+        const [ rows ] = await db.query('select * from user_technologies where user_id = ?', [req.user.userId]);//gets data from database
         
-        return res.status(200).json(rows[0].user_technologies)
+        if(rows.length < 1) return res.status(204);//checks if user does not have technologies into database and returns 204 status code
+        
+        return res.status(200).json(rows[0].user_technologies);//else returns 200 statsu code and technologies list to frotnend 
 
     }catch(err){
-        return res.status(500).json('Database Error')
-    }
 
-})
+        return res.status(500).json('Database Error'); //returns 500 status code if internal error occurs
+
+    };
+});//api is used to get my users technologeis based on myuser.id
 
 
 TechRouter.post('/new-technologies', verifyToken, rateLimiter, async (req,res) => {
     
     const TechResp = TechnologiesSchema(req.body)
 
-
-
-    if(!TechResp.success){
-        return res.status(400).json('invalid input')
-    }
+    if(!TechResp.success)return res.status(400).json('invalid input')
     
     try{
 
-        const [ rows ] = await db.query('select * from user_technologies where user_id = ?' , [req.user.userId])
+        const [ rows ] = await db.query('select * from user_technologies where user_id = ?' , [req.user.userId]); //gets data from database
 
-        if(rows.length < 1){
+        if(rows.length < 1){//statement checks if user already has technologies , if no then...
 
-            await db.query('insert into user_technologies (user_id,user_technologies) values (?,?)', [req.user.userId, JSON.stringify(req.body.technologies)])
+            await db.query('insert into user_technologies (user_id,user_technologies) values (?,?)', [req.user.userId, JSON.stringify(req.body.technologies)]);//server sends insertion statement to database
             
-            return res.status(200).json('Technologies Uploaded Successfully')
+            return res.status(200).json('Technologies Uploaded Successfully');//returns 200 stauts code to frotnend
             
-        }
+        };
 
-        await db.query('update user_technologies set user_technologies = ? where user_id = ?' , [JSON.stringify(req.body.technologies),req.user.userId])
+        //else if user already has technologies , then...
+
+        await db.query('update user_technologies set user_technologies = ? where user_id = ?' , [JSON.stringify(req.body.technologies),req.user.userId]);//server sends update statement to database
     
-        return res.status(200).json('Technologies Updated Successfully')
+        return res.status(200).json('Technologies Updated Successfully');//returns 200  status code to frotnend
     
-    }
+    }catch(err){
+        return res.status(500).json({err : 'Database Error'});//returns 500 status code to frotnend if internal error occurs
+    };
+});//api is used for employee to update / upload their technologies list to their page
 
-    catch(err){
-        return res.status(500).json({err : 'Database Error'})
-    }
-    
-
-})
-
-
-
-module.exports = TechRouter
+module.exports = TechRouter;//exporting component
