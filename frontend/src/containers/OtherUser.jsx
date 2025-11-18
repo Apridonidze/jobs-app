60751be086238647fee7201953afb283e1a513f6
unknown <giorgiapridonidze08@gmail.com>
2025-11-18 15:12:19 +0400

    cleaned up and documentated OtherUser.jsx component , implemented Error handling component

--- frontend/src/containers/OtherUser.jsx
+++ frontend/src/containers/OtherUser.jsx
@@ -1,18 +1,23 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
-import { useCookies } from "react-cookie"
-import { replace, useNavigate, useParams } from "react-router-dom"
-import OtherUserData from "./OtherUserData"
+import axios from "axios";
+import { useCookies } from "react-cookie";
+import {  useNavigate, useParams } from "react-router-dom"; //importing react libraries
+
+import { useEffect, useState } from "react"; //importing react hooks
+
+import Error from "../alerts/Error";
+import OtherUserData from "./OtherUserData"; //importing react component
 
 const OtherUser = () => {
 
-    const { userId } = useParams()
-    const [ cookies ] = useCookies(['token'])
-    const navigator = useNavigate()
+    const [ cookies ] = useCookies(['token']); //cookies
 
-    const [userData ,setUserData] = useState(null)
+    const USER_URL = 'http://localhost:8080/user/user'; //api url to fetch user data
+    
+    const { userId } = useParams(); //hook that gets url parameter when visitin user page via user_id
+    const [userData ,setUserData] = useState(null); //state to store user data from api
+    const navigator = useNavigate(); //navigator to navigate user via pages
 
-    const USER_URL = 'http://localhost:8080/user/user' // move to .env
+    const [toggleError,setToggleError] = useState(false) ; //state to toggle Error component
 
     useEffect(() => {
 
@@ -20,49 +25,47 @@ const OtherUser = () => {
 
             try{
 
-                await Promise.all([
-                    axios.get(`${USER_URL}/${userId}` , {headers : {Authorization : `Bearer ${cookies.token}`}})
-                    .then(resp => {
-                        const message = resp.data.message
-                        const status = resp.data.status
-                        const data = resp.data.data
-                        const myuser = resp.data.myuser
+                await axios.get(`${USER_URL}/${userId}` , {headers : {Authorization : `Bearer ${cookies.token}`}})
+                .then(resp => {
+                    const message = resp.data.message; //data success message
+                    const status = resp.data.status; //data success status 
+                    const data = resp.data.data; //data variable
+                    const myuser = resp.data.myuser; //checks if visiting user user_id === my_id
 
-                        if(!status) navigator('*', {replace : true})
+                    if(!status) navigator('*', {replace : true}); //if we wont get user it navigates us to main page
 
+                    if(myuser) navigator('/my-account', {replace  : true}); //if visiting account is my account then it navigates us to my-account route
 
-                        if(myuser) navigator('/my-account', {replace  : true})
-
-                        setUserData(data)
-                        
-                        
+                    setUserData(data); //else it sends user data in state
+                    setToggleError(false); //untoggles erorr component if we get data successfully
 
                     })
-                ])
+
 
             }catch(err){
 
-                console.log(err)
+                console.log(err) ; //consoles error
+                setToggleError(true); //toggles error compomnent if error occurs
 
-            }
+            };
 
-        }
+        };
 
-        FetchUserData()
+        FetchUserData(); //declearing variable
 
-    },[])
+    },[]) ; //function mounts once page is loaded
 
-    //user, user_avatar, user_desc, user_roles, user_technologies
 
     return (
         <div className="other-user-container">
+
+            {toggleError && <Error setToggleError={setToggleError}/>}
 
             {userData && <OtherUserData userData={userData} />}
           
 
         </div>
-    )
-}
+    );
+};
 
-
-export default OtherUser
+export default OtherUser; //exporting component
